c7d9b3bf73c4a69b6ab83a9e23ab91fee5571981
Pedro Sodré <107010234+PedroSodrr10@users.noreply.github.com>
2026-03-27 15:11:23 -0300

    Update links.ts

--- src/config/links.ts
+++ src/config/links.ts
@@ -1,5 +1,5 @@
 export const LINKS = {
-  whatsapp: "https://wa.me/55433325-4030",
+  whatsapp: "https://wa.me/554333254030",
   email: "mailto:inovarecebe@gmail.com",
   emailAddress: "inovarecebe@gmail.com",
   phone: "(43) 3325-4030",
